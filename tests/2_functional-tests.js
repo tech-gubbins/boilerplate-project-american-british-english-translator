f18cd82a5c0c53a06964a9b911bad3deb7834a20
Joshua McNeel <joshmcneel@gmail.com>
2024-12-17 15:38:55 -0500

    feat: finish functional tests

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -21,14 +21,14 @@ suite('Functional Tests', () => {
                     assert.equal(res.status, 200);
                     assert.property(res.body, 'text');
                     assert.property(res.body, 'translation');
-                    assert.include(res.body.translation, `<span class="highlight">favorite</span>`);
+                    assert.include(res.body.translation, `<span class="highlight">favourite</span>`);
                     done();
                 })
         });
 
         test('With text and invalid locale field', (done) => {
             chai
-                .request(server).
+                .request(server)
                 .post('/api/translate')
                 .send({
                     text: 'Mangoes are my favorite fruit.',
@@ -44,6 +44,65 @@ suite('Functional Tests', () => {
 
         test('With missing text field', (done) => {
             chai
-        })
-    })
+                .request(server)
+                .post('/api/translate')
+                .send({
+                    locale: 'american-to-british'
+                })
+                .end((err, res) => {
+                    assert.equal(res.status, 200);
+                    assert.property(res.body, 'error');
+                    assert.equal(res.body.error, 'Required field(s) missing');
+                    done();
+                })
+        });
+
+        test('With missing locale field', (done) => {
+            chai
+                .request(server)
+                .post('/api/translate')
+                .send({
+                    text: 'Mangoes are my favorite fruit'
+                })
+                .end((err, res) => {
+                    assert.equal(res.status, 200);
+                    assert.property(res.body, 'error');
+                    assert.equal(res.body.error, 'Required field(s) missing');
+                    done();
+                })
+        });
+
+        test('With empty text', (done) => {
+            chai
+                .request(server)
+                .post('/api/translate')
+                .send({
+                    text: '',
+                    locale: 'american-to-british'
+                })
+                .end((err, res) => {
+                    assert.equal(res.status, 200);
+                    assert.property(res.body, 'error');
+                    assert.equal(res.body.error, 'No text to translate');
+                    done();
+                })
+        });
+
+        test('With text that needs no translation', (done) => {
+            chai
+                .request(server)
+                .post('/api/translate')
+                .send({
+                    text: 'This text needs no translation',
+                    locale: 'american-to-british'
+                })
+                .end((err, res) => {
+                    assert.equal(res.status, 200);
+                    assert.property(res.body, 'text');
+                    assert.property(res.body, 'translation');
+                    assert.equal(res.body.translation, 'Everything looks good to me!');
+                    done();
+                })
+        });
+    });
 });
