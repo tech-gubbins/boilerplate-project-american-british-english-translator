3af17d96bba1e887d321e48696fe75cbcc07c4b6
Joshua McNeel <joshmcneel@gmail.com>
2024-12-17 15:26:41 -0500

    feat: started functional tests

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -8,5 +8,42 @@ chai.use(chaiHttp);
 let Translator = require('../components/translator.js');
 
 suite('Functional Tests', () => {
+    suite('Translation POST request to /api/translate', () => {
+        test('With text and locale fields', (done) => {
+            chai
+                .request(server)
+                .post('/api/translate')
+                .send({
+                    text: 'Mangoes are my favorite fruit',
+                    locale: 'american-to-british'
+                })
+                .end((err, res) => {
+                    assert.equal(res.status, 200);
+                    assert.property(res.body, 'text');
+                    assert.property(res.body, 'translation');
+                    assert.include(res.body.translation, `<span class="highlight">favorite</span>`);
+                    done();
+                })
+        });
 
+        test('With text and invalid locale field', (done) => {
+            chai
+                .request(server).
+                .post('/api/translate')
+                .send({
+                    text: 'Mangoes are my favorite fruit.',
+                    locale: 'invalid-locale'
+                })
+                .end((err, res) => {
+                    assert.equal(res.status, 200);
+                    assert.property(res.body, 'error');
+                    assert.equal(res.body.error, 'Invalid value for locale field');
+                    done();
+                })
+        });
+
+        test('With missing text field', (done) => {
+            chai
+        })
+    })
 });
