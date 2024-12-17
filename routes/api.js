e0034dcc1660d55974047f56a63b879a976104e9
Joshua McNeel <joshmcneel@gmail.com>
2024-12-17 14:11:42 -0500

    feat: implemented APIs

--- routes/api.js
+++ routes/api.js
@@ -3,11 +3,20 @@
 const Translator = require('../components/translator.js');
 
 module.exports = function (app) {
-  
-  const translator = new Translator();
+	const translator = new Translator();
 
-  app.route('/api/translate')
-    .post((req, res) => {
-      
-    });
+	app.route('/api/translate').post((req, res) => {
+    const { text, locale } = req.body;
+
+    if (text === undefined || locale === undefined) {
+      return res.json({ error: 'Required field(s) missing' });
+    }
+
+    if (text === '') {
+      return res.json({ error: 'No text to translate' });
+    }
+
+    const result = translator.translate(text, locale);
+    res.json(result);
+  });
 };
